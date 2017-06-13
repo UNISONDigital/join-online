deeadf1159548fe04af0b96e7bd67ead3dd44d72
Joshua Carrington <j.carrington@unison.co.uk>
2017-06-13 15:42:21 +0100

    Add salary_to_band route

--- routes/api.php
+++ routes/api.php
@@ -60,7 +60,69 @@ Route::get('/validate_ni_number/{ni_number}', function(Request $request, $ni_num
 
 // Convert raw salary to UNISON subscription band
 Route::get('/salary_to_band/{salary}', function(Request $request, $salary) {
-	return 'FINISHME';
+	
+	if ($salary && filter_var($salary, FILTER_VALIDATE_FLOAT)) {
+
+		$success = true;
+
+		switch(true) {
+			
+			case $salary <= 2000:
+				$result = ['band' => 'A', 'rate' => '£1.30'];
+				break;
+
+			case $salary <= 5000:
+				$result = ['band' => 'B', 'rate' => '£3.50'];
+				break;
+
+			case $salary <= 8000:
+				$result = ['band' => 'C', 'rate' => '£5.30'];
+				break;
+
+			case $salary <= 11000:
+				$result = ['band' => 'D', 'rate' => '£6.60'];
+				break;
+
+			case $salary <= 14000:
+				$result = ['band' => 'E', 'rate' => '£7.85'];
+				break;
+
+			case $salary <= 17000:
+				$result = ['band' => 'F', 'rate' => '£9.70'];
+				break;
+
+			case $salary <= 20000:
+				$result = ['band' => 'G', 'rate' => '£11.50'];
+				break;
+
+			case $salary <= 25000:
+				$result = ['band' => 'H', 'rate' => '£14.00'];
+				break;
+
+			case $salary <= 30000:
+				$result = ['band' => 'I', 'rate' => '£17.25'];
+				break;
+
+			case $salary <= 35000:
+				$result = ['band' => 'J', 'rate' => '£20.30'];
+				break;
+
+			default:
+				$result = ['band' => 'K', 'rate' => '£22.50'];
+				break;
+
+		}
+
+	} else {
+		$success = false;
+		$result = 'invalid_salary_provided';
+	}
+
+	return response()->json([
+		'success' => $success, 
+		'result' => $result
+	])->withCallback($request->input('callback'));
+
 });
 
 
