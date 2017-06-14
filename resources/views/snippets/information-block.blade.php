e4296ad1fb84715e44ae72b532e0f802424a421b
Tom Sansome <sansometom@gmail.com>
2017-06-14 21:47:44 +0100

    Use different placeholder text

--- resources/views/snippets/information-block.blade.php
+++ resources/views/snippets/information-block.blade.php
@@ -4,10 +4,10 @@
     <div class="information-block__points">
       <div class="information-block__image"></div>
       <ul class="information-block__items">
-        <li class="information-block__item">Cras mattis consectetur purus sit amet fermentum.</li>
-        <li class="information-block__item">Cras mattis consectetur purus sit amet fermentum.</li>
-        <li class="information-block__item">Cras mattis consectetur purus sit amet fermentum.</li>
-        <li class="information-block__item">Nullam id dolor id nibh ultricies vehicula ut id elit. Aenean lacinia bibendum nulla sed consectetur.</li>
+        <li class="information-block__item">Trained local representatives to advise and give support with problems at work</li>
+        <li class="information-block__item">Advice on health and safety issues</li>
+        <li class="information-block__item">Training in professional development and life skills</li>
+        <li class="information-block__item">Note: UNISON may not be able to offer legal advice and representation for an existing problem at work (see ‘after four weeks membership’ for more information)</li>
       </ul>
     </div>
   </div>
