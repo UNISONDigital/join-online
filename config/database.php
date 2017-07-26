cd0ef3a750586d58bbeda4f0f7b7f80ec4ba150d
Rich Shakespeare <richshakespeare@hotmail.co.uk>
2017-07-26 14:48:30 +0100

    make default db sensible

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'postgres'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'join'),
             'username' => env('DB_USERNAME', 'postgres'),
