1cadc9c2b1eaf12ede675c8538ec3ccd959c498c
Rich Shakespeare <richshakespeare@hotmail.co.uk>
2017-08-03 13:37:19 +0100

    add more columns

--- database/migrations/2017_07_27_122754_create_applications_table.php
+++ database/migrations/2017_07_27_122754_create_applications_table.php
@@ -14,40 +14,58 @@ class CreateApplicationsTable extends Migration
      */
     public function up()
     {
-        Schema::create('applications', function (Blueprint $table) {
-            $table->increments('id');
-            $table->text('token');
-                        
-            // step 1
-            $table->string('title')->nullable();
-            $table->string('first_name')->nullable();
-            $table->string('last_name')->nullable();
-            $table->string('email')->nullable();
-            $table->string('date_of_birth')->nullable();
-            $table->string('national_insurance')->nullable();
+				Schema::create('applications', function (Blueprint $table) {
+				$table->increments('id');
+				$table->text('token');
 
-            // step 2
-            $table->string('address_1')->nullable();
-            $table->string('address_2')->nullable();
-            $table->string('address_3')->nullable();
-            $table->string('town')->nullable();
-            $table->string('postcode')->nullable();    
-            $table->string('work_email')->nullable();
+				// step 1
+				$table->string('title')->nullable();
+				$table->string('first_name')->nullable();
+				$table->string('last_name')->nullable();
+				$table->string('email')->nullable();
+				$table->string('date_of_birth')->nullable();
+				$table->string('national_insurance')->nullable();
 
-            // step 3
-            $table->string('employer')->nullable();
-            $table->string('workplace')->nullable();
-            $table->string('salary')->nullable();
-            $table->string('salary_frequency')->nullable();
+				// step 2
+				$table->string('address_1')->nullable();
+				$table->string('address_2')->nullable();
+				$table->string('address_3')->nullable();
+				$table->string('town')->nullable();
+				$table->string('postcode')->nullable();    
+				$table->string('work_email')->nullable();
 
-            // step 4
-            $table->string('payment_method')->nullable();
-            $table->string('direct_debit_account_name')->nullable();
-            $table->string('direct_debit_account_number')->nullable();
-            $table->string('direct_debit_sort_code')->nullable();
-            $table->string('direct_debit_day_of_month')->nullable();
+				// step 3
+				$table->string('employer')->nullable();
+				$table->string('workplace')->nullable();
+				$table->integer('employer_id')->nullable();
+				$table->integer('workplace_id')->nullable();
 
-            $table->timestamps();
+				$table->string('job_title')->nullable();
+				$table->string('salary')->nullable();
+				$table->string('salary_frequency')->nullable();
+				$table->string('hours_per_week')->nullable();
+
+				// step 4
+				$table->string('payment_method')->nullable();
+				$table->string('direct_debit_account_name')->nullable();
+				$table->string('payroll_consent')->nullable();
+				$table->string('direct_debit_account_number')->nullable();
+				$table->string('direct_debit_sort_code')->nullable();
+				$table->string('direct_debit_day_of_month')->nullable();
+
+				// step 5
+				$table->string('communications_method')->nullable();
+				$table->string('communications_type')->nullable();
+				$table->string('fund')->nullable();
+				$table->string('gender')->nullable();
+				$table->string('disability')->nullable();
+				$table->string('orientation')->nullable();
+				$table->string('ethnicity')->nullable();
+
+				// account
+				$table->string('password')->nullable();
+
+				$table->timestamps();
         });
 
     }
